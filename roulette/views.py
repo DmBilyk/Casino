3aae3279f6c3c43d5720625099c2157c48ad8101
DmBilyk <dmbilyk3861@gmail.com>
2024-10-12 23:01:26 +0300

    settings.py

--- roulette/views.py
+++ roulette/views.py
@@ -2,32 +2,28 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import random
 import json
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.decorators import login_required
-from casino_main.models import Profile
 
+def initialize_session(request):
+    if 'balance' not in request.session:
+        request.session['balance'] = 1000
 
-
-@login_required
 def roulette(request):
-    profile, created = Profile.objects.get_or_create(user=request.user)
+    initialize_session(request)
     context = {
-        'balance': profile.balance,
+        'balance': request.session['balance'],
     }
-
     return render(request, 'roulette/roulette.html', context)
 
-@login_required
 def spin(request):
-    profile, created = Profile.objects.get_or_create(user=request.user)
+    initialize_session(request)
 
     bets = json.loads(request.POST.get('bets', '[]'))
     total_bet_amount = sum(bet['amount'] for bet in bets)
 
-    if profile.balance < total_bet_amount:
+    if request.session['balance'] < total_bet_amount:
         return JsonResponse({'error': 'Insufficient balance'}, status=400)
 
-    profile.balance -= total_bet_amount
+    request.session['balance'] -= total_bet_amount
 
     result = random.randint(0, 36)
     color = 'red' if result in [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36] else 'black' if result != 0 else 'green'
@@ -43,11 +39,10 @@ def spin(request):
         elif bet['type'].startswith('number') and int(bet['type'].split()[1]) == result:
             win_amount += bet['amount'] * 35
 
-    profile.balance += win_amount
-    profile.save()
+    request.session['balance'] += win_amount
 
     return JsonResponse({
-        'result': result,
-        'balance': profile.balance,
+        'result': f"{result} {color}",
+        'balance': request.session['balance'],
         'win_amount': win_amount,
     })
